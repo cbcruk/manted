54966d9e9ec1f5b09b47970a78dcf4a24c469c01
cbcruk <cbcruk@users.noreply.github.com>
2021-06-27 08:48:40 +0900

    :lipstick: update: 스크린 사이즈가 작을 경우 메모영역 스크롤 활성화

--- src/components/Memo/Memo.jsx
+++ src/components/Memo/Memo.jsx
@@ -29,15 +29,17 @@ function Memo() {
   } = selected
 
   return (
-    <div className="fixed inset-2 top-auto md:top-3 md:right-3 md:bottom-auto md:left-auto z-10 md:w-72 rounded-md overflow-hidden bg-white shadow-md">
-      <div className="flex md:flex-col">
-        <Hero {...{ id, company_id, logo_img_thumb, title_img_thumb }} />
-        <div className="flex-1">
-          <Summary {...{ name, industry_name, position }} />
-          <Features {...{ avg_day, avg_rate, like_count }} />
+    <div className="max-h-screen fixed inset-0 md:top-0 top-auto md:left-auto z-10 p-2">
+      <div className="md:w-72 md:max-h-full rounded-md md:overflow-auto overflow-hidden bg-white shadow-md">
+        <div className="flex md:flex-col bg-white">
+          <Hero {...{ id, company_id, logo_img_thumb, title_img_thumb }} />
+          <div className="flex-1">
+            <Summary {...{ name, industry_name, position }} />
+            <Features {...{ avg_day, avg_rate, like_count }} />
+          </div>
         </div>
+        <Desc {...{ main_tasks, requirements }} />
       </div>
-      <Desc {...{ main_tasks, requirements }} />
     </div>
   )
 }
