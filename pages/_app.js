6a298803d58d4abfd1ad7d7744f77f1347fdda7a
cbcruk <cbcruk@gmail.com>
2021-06-28 19:17:38 +0900

    :zap: improvement: github 데이터 로딩 방식으로 변경

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,11 @@
 import { Provider } from 'jotai'
-import { SWRConfig } from 'swr'
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 
 function App({ Component, pageProps }) {
   return (
     <Provider>
-      <SWRConfig
-        value={{
-          revalidateOnFocus: false,
-        }}
-      >
-        <Component {...pageProps} />
-      </SWRConfig>
+      <Component {...pageProps} />
     </Provider>
   )
 }
--- pages/api/jobs/index.js
+++ pages/api/jobs/index.js
@@ -1,6 +1,6 @@
 async function jobs(_req, res) {
   const response = await fetch(
-    `https://script.google.com/macros/s/${process.env.SPREADSHEET_ID}/exec?type=json`
+    'https://raw.githubusercontent.com/cbcruk/manted/main/raw_data/data.json'
   )
   const data = await response.json()
 
