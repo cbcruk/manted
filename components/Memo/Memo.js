b6dece4c6c4807b20dd576a8028d1c1eb456cc4c
cbcruk <cbcruk@gmail.com>
2021-06-29 06:00:55 +0900

    :bug: bug: memo 스크롤 속성 수정

--- components/Memo/Memo.js
+++ components/Memo/Memo.js
@@ -29,7 +29,7 @@ function Memo() {
 
   return (
     <div className="max-h-screen fixed inset-0 md:top-0 top-auto md:bottom-auto md:left-auto z-10 p-2">
-      <div className="md:w-72 md:max-h-full rounded-md md:overflow-auto overflow-hidden bg-white shadow-md">
+      <div className="md:w-72 md:max-h-[calc(100vh-1.5rem)] rounded-md md:overflow-auto overflow-hidden bg-white shadow-md">
         <div className="flex md:flex-col bg-white">
           <Hero {...{ id, company_id, logo_img_thumb, title_img_thumb }} />
           <div className="flex-1 overflow-hidden">
